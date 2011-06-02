71cf1ec4abc843d8b453f48959a7cc7e12d195c9
Nate Vack <njvack@freshforever.net>
2011-06-02 08:17:30 -0500

    Add CSV upload handling

--- scatterize.py
+++ scatterize.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 import flask
+from flask import g, request
 import time
-import ols
-
+import csv
 import numpy as np
+
+import ols
 
 app = flask.Flask(__name__)
 
 ASSET_TIME = int(time.time())
+STORAGE_DIR = "/Users/njvack/tmp/scatterize"
 
 def add_time_to_global():
-    flask.g.time = ASSET_TIME
+    g.time = ASSET_TIME
 
 app.before_request(add_time_to_global)
 
@@ -25,26 +28,77 @@ def scatter():
     b0 = np.random.uniform(-20, 20)
     b1 = np.random.uniform(-20, 20)
     x = np.linspace(0, 50, num=points)
-    y = b0*np.ones(points)+b1*x+np.random.uniform(0, er_sd, points)
+    y = (b0*np.ones(points))+(b1*x)
+    err = np.random.normal(loc=0, scale=20, size=points)
+    y += err
     
     params = {
         'n' : points,
-        'er_sd': er_sd,
         'b0' : b0,
-        'b1' : b1
+        'b1' : b1,
+        'err': err.tolist()
     }
     
     points = np.column_stack((x,y)).tolist()
-    return flask.jsonify(params=params, points=points)
+    r = ols.ols(y, np.column_stack((np.ones_like(x), x)), 'y', ['const', 'x'])
+    regression_data = {
+        'b' : r.b.tolist(),
+        't' : r.t.tolist(),
+        'p' : r.p.tolist()
+    }
+    
+    return flask.jsonify(params=params, points=points, result=regression_data)
 
 @app.route("/")
 def index():
     return flask.render_template("index.html")
 
-@app.route("/g/<filehash>")
-def show_thing(f):
-    pass
-        
+@app.route("/d", methods=["POST"])
+def upload():
+    import hashlib
+    file_raw = request.files['csvfile']
+    lines = file_raw.read().splitlines()
+    dialect = csv.Sniffer().sniff("\n".join(lines[0:5]))
+    reader = csv.reader(lines, dialect=dialect)
+    h = hashlib.md5()
+    rows = list(reader)
+    h.update(str(rows))
+    filename = "%s.csv" % (h.hexdigest())
+    with open("%s/%s" % (STORAGE_DIR, filename), 'w') as outfile:
+        writer = csv.writer(outfile, dialect="excel")
+        writer.writerows(rows)
+    g.filename = filename
+    g.rows = rows
+    return flask.render_template("uploaded.html")
+
+@app.route("/d/<filehash>")
+def scatter_frame(filehash):
+    rows = []
+    with open("%s/%s.csv" % (STORAGE_DIR, filehash), 'rt') as csvfile:
+        reader = csv.reader(csvfile, dialect="excel")
+        rows = list(reader)
+    g.rows = rows
+    g.filehash = filehash
+    
+    return flask.render_template("scatter_frame.html")
+
+@app.route("/d/<filehash>/regress.js")
+def regress_js(filehash):
+    filename = "%s/%s.csv" % (STORAGE_DIR, filehash)
+    with open(filename, 'rt') as csvfile:
+        reader = csv.reader(csvfile, dialect="excel")
+        headers = reader.next()
+        csvfile.seek(0)
+        datas = np.genfromtxt(csvfile, delimiter=",", skip_header=1)
+    
+    xvals = datas[:,0]
+    yvals = datas[:,3]
+    mA = np.column_stack((np.ones_like(xvals), xvals))
+    result = ols.ols(yvals, mA, 'y', ['const', 'slope'])
+    points = np.column_stack((xvals, yvals)).tolist()
+    return flask.jsonify(points=points, 
+        b=result.b.tolist(), p=result.p.tolist(), t=result.t.tolist())
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
