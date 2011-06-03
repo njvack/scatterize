e8edf31d46e10d94714dcbff2414726755bbeacb
Nate Vack <njvack@freshforever.net>
2011-06-03 14:39:57 -0500

    Add regression line(s) to scatterplots

--- static/js/scatterize.js
+++ static/js/scatterize.js
@@ -13,18 +13,30 @@ var S = {
     
     pub.set_points = function(points) {
       pub.points = points;
+      my.xvals = points.map(function(p) {return p[0]; });
+      my.yvals = points.map(function(p) {return p[1]; });
+    };
+    
+    pub.draw_points = function(points) {
       pub.vis.add(pv.Dot)
         .data(points)
         .left(function(p) {return my.x(p[0]);})
-        .bottom(function(p){return my.y(p[1]);});
-    };
+        .bottom(function(p) {return my.y(p[1]);});
+    }
     
     my.set_scales = function(points) {
-      var xvals = points.map(function(p) {return p[0]; });
-      var yvals = points.map(function(p) {return p[1]; });
-      my.x = pv.Scale.linear(pv.min(xvals), pv.max(xvals)).range(0, w);
-      my.y = pv.Scale.linear(pv.min(yvals), pv.max(yvals)).range(0, h);
+      my.x = pv.Scale.linear(pv.min(my.xvals), pv.max(my.xvals)).range(0, w);
+      my.y = pv.Scale.linear(pv.min(my.yvals), pv.max(my.yvals)).range(0, h);
     };
+    
+    pub.regression_line = function(slope, intercept, color) {
+      if (!color) { color = "#000"; }
+      pub.vis.add(pv.Line)
+        .data(my.xvals)
+        .strokeStyle(color)
+        .bottom(function(xval) { return my.y((slope*xval)+intercept);})
+        .left(function(xval) { return my.x(xval);});
+    }
     
     my.add_rules = function() {
       pub.vis.add(pv.Rule)
@@ -38,9 +50,10 @@ var S = {
     }
     
     pub.draw_data = function(points) {
-      my.set_scales(points)
-      my.add_rules()
-      pub.set_points(points)
+      pub.set_points(points);
+      my.set_scales(points);
+      my.add_rules();
+      pub.draw_points(points)
       pub.vis.render();
     }
     
