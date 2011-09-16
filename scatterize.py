02104658f86cbad606c420fd835b114718aa815c
Nate Vack <njvack@freshforever.net>
2011-09-16 13:25:29 -0500

    Refactor regression paramater parsing

--- scatterize.py
+++ scatterize.py
@@ -4,7 +4,7 @@
 # Part of scatterize -- a statistical exploration tool
 #
 # Copyright (c) 2011 Board of Regents of the University of Wisconsin System
-# 
+#
 # scatterize is licensed under the GPLv3 -- see LICENSE for details.
 #
 # Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
@@ -19,7 +19,7 @@ import numpy as np
 
 from utils import add_url_helpers
 from stats import statsrunner, statsfile
-from stats.statsrunner import StatsRunner
+from stats.statsrunner import RegressionParams, StatsRunner
 from stats.statsfile import CSVFileHandler
 
 import wsgi_utils
@@ -33,35 +33,40 @@ add_url_helpers(app)
 statsrunner.logger = app.logger
 statsfile.logger = app.logger
 
+
 @app.route("/")
 def index():
     return flask.render_template("index.html")
-    
+
+
 @app.route("/about")
 def about():
     return flask.render_template("about.html")
 
+
 @app.route("/save_svg", methods=["GET", "POST"])
 def save_svg():
     preamble = """<?xml version="1.0" standalone="no"?>
-
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 """
     svgdata = request.form.get("svg_data", "")
     response = flask.make_response(preamble+svgdata)
     response.headers["Content-Disposition"] = "attachment; filename=plot.svg"
     return response
 
+
 @app.route("/d", methods=["POST"])
 def upload():
     file_raw = request.files['csvfile']
     file_handler = CSVFileHandler(settings.STORAGE_DIR)
     short_hash = file_handler.save_upload(
-        request.files['csvfile'], 
+        request.files['csvfile'],
         hash_len=settings.HASH_PREFIX_CHARS,
         sniff_lines=settings.SNIFF_LINES)
 
     return flask.redirect(flask.url_for('scatter_frame', filehash=short_hash))
+
 
 @app.route("/d/<filehash>")
 def scatter_frame(filehash):
@@ -73,8 +78,9 @@ def scatter_frame(filehash):
         flask.abort(404)
     g.column_names = stats_data.column_names
     g.rows = stats_data.data_list
-    
+
     return flask.render_template("scatter_frame.html")
+
 
 @app.route("/d/<filehash>/regress.js")
 def regress_js(filehash):
@@ -83,28 +89,16 @@ def regress_js(filehash):
         stats_data = file_handler.load_file(filehash)
     except:
         flask.abort(404)
-    x_idx = int(request.args.get("x", 0))
-    y_idx = int(request.args.get("y", 0))
-    nuis_idxs = []
-    nlist = request.args.get("n", "").strip()
-    if nlist != "":
-        nuis_idxs = [int(i) for i in nlist.split(",")]
-    
-    censor_idxs = []    
-    clist = request.args.get("c", "").strip()
-    if clist != "":
-        censor_idxs = [int(i) for i in clist.split(",")]
-    
-    mtype = request.args.get("m", "OLS")
-    highlight_idx = request.args.get("h", None)
-    
-    sr = StatsRunner(stats_data, y_idx, x_idx, nuis_idxs, 
-        highlight_idx, censor_idxs, mtype)
+
+    regression_params = RegressionParams.build_from_flask_args(request.args)
+    sr = StatsRunner(stats_data, regression_params)
     result = sr.run()
+
     # Trim a couple things from the result -- won't need 'em
     del result['all_point_data']
     del result['all_point_cols']
     return flask.jsonify(result)
+
 
 @app.route("/d/<filehash>/regress.csv")
 def regress_csv(filehash):
@@ -114,38 +108,22 @@ def regress_csv(filehash):
         stats_data = file_handler.load_file(filehash)
     except:
         flask.abort(404)
-    
-    x_idx = int(request.args.get("x", 0))
-    y_idx = int(request.args.get("y", 0))
-    nuis_idxs = []
-    nlist = request.args.get("n", "").strip()
-    if nlist != "":
-        nuis_idxs = [int(i) for i in nlist.split(",")]
-    
-    censor_idxs = []    
-    clist = request.args.get("c", "").strip()
-    if clist != "":
-        censor_idxs = [int(i) for i in clist.split(",")]
-    
-    mtype = request.args.get("m", "OLS")
-    highlight_idx = request.args.get("h", None)
-    
-    sr = StatsRunner(stats_data, y_idx, x_idx, nuis_idxs, 
-        highlight_idx, censor_idxs, mtype)
+
+    regression_params = RegressionParams.build_from_flask_args(request.args)
+    sr = StatsRunner(stats_data, regression_params)
     result = sr.run()
-    
+
     # Build us a string in memory
     out_buf = StringIO.StringIO()
-    writer = csv.writer(out_buf, delimiter=",", quotechar='"', 
-        quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(out_buf, dialect='excel')
     writer.writerow(result['all_point_cols'])
-    for row in result['all_point_data']:
-        writer.writerow(row)
-    
+    writer.writerows(result['all_point_data'])
+
     response = flask.make_response(out_buf.getvalue())
     response.headers["Content-Disposition"] = "attachment;filename=data.csv"
     response.headers["Content-Type"] = "text/csv"
     return response
+
 
 if __name__ == "__main__":
     app.debug = True
--- stats/statsrunner.py
+++ stats/statsrunner.py
@@ -2,7 +2,7 @@
 # Part of scatterize -- a statistical exploration tool
 #
 # Copyright (c) 2011 Board of Regents of the University of Wisconsin System
-# 
+#
 # scatterize is licensed under the GPLv3 -- see LICENSE for details.
 #
 # Written by Nathan Vack <njvack@wisc.edu> at the Waisman Laborotory
@@ -20,67 +20,96 @@ import logging
 logger = logging.getLogger("statsrunner")
 
 
-class StatsRunner(object):
-    """The thing you'll use to actually run your statistics."""
-    def __init__(self, stats_data, dv_idx, iv_idx, nuis_idxs, 
-            highlight_idx, censor_rows, model="OLS", model_options={}):
-        super(StatsRunner, self).__init__()
-        self.stats_data = stats_data
+class RegressionParams(object):
+    """
+    The parameters we're going to feed to our StatsRunner -- contains
+    everything necessary to specify an analysis.
+    """
+
+    def __init__(self, model_type, dv_idx, iv_idx, nuis_idxs, highlight_idx,
+        censor_idxs):
+
+        self.model_type = model_type
         self.dv_idx = dv_idx
         self.iv_idx = iv_idx
         self.nuis_idxs = nuis_idxs
         self.highlight_idx = highlight_idx
-        self.censor_rows = censor_rows
-        self.model = model
-        self.model_options = model_options
+        self.censor_idxs = censor_idxs
+
+    @classmethod
+    def build_from_flask_args(cls, args):
+        x_idx = int(args.get("x", 0))
+        y_idx = int(args.get("y", 0))
+        nuis_idxs = []
+        nlist = args.get("n", "").strip()
+        if nlist != "":
+            nuis_idxs = [int(i) for i in nlist.split(",")]
+
+        censor_idxs = []
+        clist = args.get("c", "").strip()
+        if clist != "":
+            censor_idxs = [int(i) for i in clist.split(",")]
+
+        mtype = args.get("m", "OLS")
+        highlight_idx = args.get("h", None)
+
+        return cls(mtype, y_idx, x_idx, nuis_idxs, highlight_idx, censor_idxs)
+
+
+class StatsRunner(object):
+    """The thing you'll use to actually run your statistics."""
+
+    def __init__(self, stats_data, regression_params):
+        super(StatsRunner, self).__init__()
+        self.stats_data = stats_data
+        self.regression_params = regression_params
 
     def run(self):
         """
         Reads a CSV file and runs some stats on it.
         """
         model_types = {
-            'OLS' : {
-                'model_fx' : self._run_ols,
-                'json_fx'  : self._jsonify_ols
-            },
-            'RLM' : {
-                'model_fx' : self._run_rlm,
-                'json_fx'  : self._jsonify_rlm
-            }
-        }
+            'OLS': {
+                'model_fx': self._run_ols,
+                'json_fx': self._jsonify_ols},
+            'RLM': {
+                'model_fx': self._run_rlm,
+                'json_fx': self._jsonify_rlm}}
+
         data_array = self.stats_data.data_array
         column_names = self.stats_data.column_names
-        
+        params = self.regression_params
+
         row_count = data_array.shape[0]
-        modeled_idxs = [self.dv_idx, self.iv_idx] + self.nuis_idxs
-        modeled_data = data_array[:,modeled_idxs]
+        modeled_idxs = [params.dv_idx, params.iv_idx] + params.nuis_idxs
+        modeled_data = data_array[:, modeled_idxs]
         possible_rows = np.all(np.isfinite(modeled_data), axis=1) # A mask!
         filtered_data = data_array[possible_rows]
         # The UI forces our censor_rows to be relative to possible_rows.
         logger.debug(filtered_data)
-        censor_ar = np.zeros((row_count, len(self.censor_rows)))
-        for i, r in enumerate(self.censor_rows):
+        censor_ar = np.zeros((row_count, len(params.censor_idxs)))
+        for i, r in enumerate(params.censor_idxs):
             censor_ar[r][i] = 1.
         filtered_censor_ar = censor_ar[possible_rows]
-        
-        dv = filtered_data[:,self.dv_idx]
+
+        dv = filtered_data[:, params.dv_idx]
         const_term = np.ones_like(dv)
-        iv = filtered_data[:,self.iv_idx]
-        nuisance_v = filtered_data[:,self.nuis_idxs]
+        iv = filtered_data[:, params.iv_idx]
+        nuisance_v = filtered_data[:, params.nuis_idxs]
         weights = np.ones_like(dv).astype(np.int)
         X = np.column_stack((const_term, iv, nuisance_v, filtered_censor_ar))
         X_nocen = np.column_stack((const_term, iv, nuisance_v))
         logger.debug(X)
-        
-        self.result = model_types[self.model]['model_fx'](dv, X)
+
+        self.result = model_types[params.model_type]['model_fx'](dv, X)
         result = self.result
-        result_nocen = model_types[self.model]['model_fx'](dv, X_nocen)
+        result_nocen = model_types[params.model_type]['model_fx'](dv, X_nocen)
         plot_resid = result.resid.copy()
-        plot_resid[self.censor_rows] = result_nocen.resid[self.censor_rows]
+        plot_resid[params.censor_idxs] = result_nocen.resid[params.censor_idxs]
         # This is ugly! Must refactor.
-        if (self.model == "RLM"):
+        if (params.model_type == "RLM"):
             weights = result.weights
-        weights[self.censor_rows] = 0
+        weights[params.censor_idxs] = 0
 
         plot_yvals = result.params[0]+(result.params[1]*iv)+plot_resid
         #point_groups = np.array(group_assignment)[possible_rows]
@@ -92,19 +121,20 @@ class StatsRunner(object):
         self.points = np.column_stack((iv, plot_yvals, weights, point_groups))
         self.all_point_data = np.column_stack((self.points, dv, X))
 
-        self.all_point_cols = ['x', 'y', 'weight', 'group', 
-            'dv_%s' % column_names[self.dv_idx], 
+        self.all_point_cols = ['x', 'y', 'weight', 'group',
+            'dv_%s' % column_names[params.dv_idx],
             'const',
-            'iv_%s' % column_names[self.iv_idx]]
-        
-        for i, n_idx in enumerate(self.nuis_idxs):
+            'iv_%s' % column_names[params.iv_idx]]
+
+        for i, n_idx in enumerate(params.nuis_idxs):
             self.all_point_cols.append(
                 "nuis_%s_%s" % (i, column_names[n_idx]))
-        for i in range(len(self.censor_rows)):
+        for i in range(len(params.censor_idxs)):
             self.all_point_cols.append("censor_%s" % i)
-        
-        return model_types[self.model]['json_fx'](self.result, self.points)
-        
+
+        return model_types[params.model_type]['json_fx'](
+            self.result, self.points)
+
     def _map_point_groups(self, data_iter, group_idx):
         gi_int = -1
         try:
@@ -124,10 +154,10 @@ class StatsRunner(object):
             else:
                 group_assignment.append(0)
         return (group_assignment, group_map)
-        
+
     def _run_ols(self, endog, exog):
         return sm.OLS(endog, exog).fit()
-        
+
     def _jsonify_ols(self, model_result, points):
         mr = model_result
         coef_result = {}
@@ -137,19 +167,18 @@ class StatsRunner(object):
             'p'         : json_float(mr.pvalues[0]),
             'se'        : json_float(mr.bse[0]),
             'col_idx'   : None,
-            'name'      : "Constant"
-        }
-        
+            'name'      : "Constant"}
+
         coef_result['x'] = {
             'b'         : json_float(mr.params[1]),
             't'         : json_float(mr.tvalues[1]),
             'p'         : json_float(mr.pvalues[1]),
             'se'        : json_float(mr.bse[1]),
-            'col_idx'   : self.iv_idx,
-            'name'      : self.stats_data.column_names[self.iv_idx]
-        }
-        
-        for i, col_idx in enumerate(self.nuis_idxs):
+            'col_idx'   : self.regression_params.iv_idx,
+            'name'      : self.stats_data.column_names[
+                self.regression_params.iv_idx]}
+
+        for i, col_idx in enumerate(self.regression_params.nuis_idxs):
             res_i = i+2
             coef_result["n_%s" % col_idx] = {
                 'b'  : json_float(mr.params[res_i]),
@@ -157,27 +186,24 @@ class StatsRunner(object):
                 'p'  : json_float(mr.pvalues[res_i]),
                 'se' : json_float(mr.bse[res_i]),
                 'col_idx' : col_idx,
-                'name' : self.stats_data.column_names[col_idx]
-            }
-        
+                'name' : self.stats_data.column_names[col_idx]}
+
         model_result = {
             "Rsq"    : json_float(mr.rsquared),
             "RsqAdj" : json_float(mr.rsquared_adj),
             "F"      : json_float(mr.fvalue),
-            "Fpv"    : json_float(mr.f_pvalue)
-        }
-        
-        return dict(points=points.tolist(), coef_result=coef_result, 
-            model_result=model_result, 
+            "Fpv"    : json_float(mr.f_pvalue)}
+
+        return dict(points=points.tolist(), coef_result=coef_result,
+            model_result=model_result,
             all_point_data=self.all_point_data.tolist(),
             all_point_cols=self.all_point_cols)
-        
 
     def _run_rlm(self, endog, exog):
         rlm_model = sm.RLM(endog, exog, M=sm.robust.norms.HuberT())
-        
+
         return rlm_model.fit()
-        
+
     def _jsonify_rlm(self, model_result, points):
         mr = model_result
         coef_result = {}
@@ -187,19 +213,18 @@ class StatsRunner(object):
             'p'         : json_float(mr.pvalues[0]),
             'se'        : json_float(mr.bse[0]),
             'col_idx'   : None,
-            'name'      : "Constant"
-        }
-        
+            'name'      : "Constant"}
+
         coef_result['x'] = {
             'b'         : json_float(mr.params[1]),
             't'         : json_float(mr.tvalues[1]),
             'p'         : json_float(mr.pvalues[1]),
             'se'        : json_float(mr.bse[1]),
-            'col_idx'   : self.iv_idx,
-            'name'      : self.stats_data.column_names[self.iv_idx]
-        }
-        
-        for i, col_idx in enumerate(self.nuis_idxs):
+            'col_idx'   : self.regression_params.iv_idx,
+            'name'      : self.stats_data.column_names[
+                self.regression_params.iv_idx]}
+
+        for i, col_idx in enumerate(self.regression_params.nuis_idxs):
             res_i = i+2
             coef_result["n_%s" % col_idx] = {
                 'b'  : json_float(mr.params[res_i]),
@@ -207,18 +232,15 @@ class StatsRunner(object):
                 'p'  : json_float(mr.pvalues[res_i]),
                 'se' : json_float(mr.bse[res_i]),
                 'col_idx' : col_idx,
-                'name' : self.stats_data.column_names[col_idx]
-            }
-        
+                'name' : self.stats_data.column_names[col_idx]}
+
         model_result = {
             "Rsq"    : json_float(np.nan),
             "RsqAdj" : json_float(np.nan),
             "F"      : json_float(np.nan),
-            "Fpv"    : json_float(np.nan)
-        }
-        
-        return dict(points=points.tolist(), coef_result=coef_result, 
+            "Fpv"    : json_float(np.nan)}
+
+        return dict(points=points.tolist(), coef_result=coef_result,
             model_result=model_result,
             all_point_data=self.all_point_data.tolist(),
             all_point_cols=self.all_point_cols)
-        
