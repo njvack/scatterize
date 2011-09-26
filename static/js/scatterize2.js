d00b5802ea0c2ce6cadf860b71ba9c396d29ff45
Nate Vack <njvack@freshforever.net>
2011-09-26 17:55:03 -0500

    Fix spacing, add ticks.

--- static/js/scatterize2.js
+++ static/js/scatterize2.js
@@ -59,7 +59,8 @@ var S2 = function($, d3) {
   S_my.scatterplot = function(container, 
       data_width, 
       data_height, 
-      data_outer_margin,
+      top_outer_margin,
+      right_outer_margin,
       axis_margin, 
       label_width,
       x_label_margin,
@@ -73,42 +74,70 @@ var S2 = function($, d3) {
     // initialization code
     my.data_width = data_width;
     my.data_height = data_height;
-    my.data_outer_margin = data_outer_margin;
+    my.top_outer_margin = top_outer_margin;
+    my.right_outer_margin = right_outer_margin;
     my.label_width = label_width;
     my.axis_margin = axis_margin;
     my.x_label_margin = x_label_margin;
     my.y_label_margin = y_label_margin;
     my.colormap = colormap;
-    my.height = (data_outer_margin + my.data_height + 
-      my.axis_margin + my.label_width + my.x_label_margin);
-    my.width = (data_outer_margin + my.data_width + 
-      my.axis_margin + my.label_width + my.y_label_margin);
-    my.data_canvas_trans_y = my.data_outer_margin;
+    my.height = (top_outer_margin + my.data_height + 
+      2*my.axis_margin + my.label_width + my.x_label_margin);
+    my.width = (right_outer_margin + my.data_width + 
+      2*my.axis_margin + my.label_width + my.y_label_margin);
+    my.data_canvas_trans_y = my.top_outer_margin;
     my.data_canvas_trans_x = my.x_label_margin + my.label_width + 2*my.axis_margin;
+    my.duration = 500;
     
     svg.attr('height', my.height)
       .attr('width', my.width)
-      .attr("xmlns", "http://www.w3.org/2000/svg");
-    
+      .attr('xmlns', 'http://www.w3.org/2000/svg')
+      .attr('text-anchor', 'middle')
+      .attr('font-size', '10px');
     
     my.data_canvas = svg.append('svg:g')
       .attr('transform', 'translate('+my.data_canvas_trans_x+','+my.data_canvas_trans_y+')')
       .attr('id', 'data-canvas');
-      
-    my.data_rect = my.data_canvas
-      .append('svg:rect')
-      .attr('width', my.data_width)
-      .attr('height', my.data_height)
-      .attr('fill', '#FAFAFA');
     
+    my.yaxis_canvas = my.data_canvas.append('svg:g')
+      .attr('transform', 'translate('+ -my.axis_margin +')')
+      .attr('id', 'y-axis')
+      .attr('shape-rendering', 'crispEdges');
+    
+    my.yaxis_canvas.append('svg:line')
+      .attr('id', 'y-axis-frame')
+      .attr('x1', 0)
+      .attr('x2', 0)
+      .attr('y1', -1)
+      .attr('y2', my.data_height)
+      .attr('stroke', 'black')
+      .attr('stroke-width', '1')
+      .attr('shape-rendering', 'crispEdges');
+    
+    my.xaxis_canvas = my.data_canvas.append('svg:g')
+      .attr('transform', 'translate(0, '+ (my.data_height+my.axis_margin)+')')
+      .attr('id', 'x-axis')
+      .attr('shape-rendering', 'crispEdges');
+          
+    my.xaxis_canvas.append('svg:line')
+      .attr('id', 'x-axis-frame')
+      .attr('x1', 0)
+      .attr('x2', my.data_width+1)
+      .attr('y1', 0)
+      .attr('y2', 0)
+      .attr('stroke', 'black')
+      .attr('stroke-width', '1')
+      .attr('shape-rendering', 'crispEdges');
+      
     pub.update = function(points, regression, xlabel, ylabel) {
       // maybe the only public function?
       
       my.set_points(points);
       my.set_regression(regression);
       my.set_scales();
-      my.draw_dots();
       my.draw_regression();
+      my.draw_dots();
+      my.draw_axes();
     }
     
     my.set_points = function(points) {
@@ -147,7 +176,7 @@ var S2 = function($, d3) {
       
       dots.transition()
         .sort(function(d) { return d.x; })
-        .duration(400)
+        .duration(my.duration)
         .attr('cx', function(d) { return my.x_scale(d.x); })
         .attr('cy', function(d) { return my.y_scale(d.y); });
       
@@ -166,16 +195,17 @@ var S2 = function($, d3) {
     my.draw_regression = function() {
       var x1, x2, line;
       
-      x1 = my.x_scale(d3.min(my.xvals));
-      x2 = my.x_scale(d3.max(my.xvals));
+      x1 = d3.min(my.xvals);
+      x2 = d3.max(my.xvals);
 
-      line = my.data_canvas.selectAll('line')
+      line = my.data_canvas.selectAll('line#regression-line')
         .data(my.regression_params);
         
       line.enter().append('svg:line')
-        .attr('x1', x1)
+        .attr('id', 'regression-line')
+        .attr('x1', my.x_scale(x1))
         .attr('y1', function(d) { return my.y_scale(d['const'] + x1*d.slope); })
-        .attr('x2', x2)
+        .attr('x2', my.x_scale(x2))
         .attr('y2', function(d) { return my.y_scale(d['const'] + x2*d.slope); });
 
       line
@@ -184,15 +214,95 @@ var S2 = function($, d3) {
         .attr('stroke-linecap', 'round');
       
       line.transition()
-        .attr('x1', x1)
+        .duration(my.duration)
+        .attr('x1', my.x_scale(x1))
         .attr('y1', function(d) { return my.y_scale(d['const'] + x1*d.slope); })
-        .attr('x2', x2)
+        .attr('x2', my.x_scale(x2))
         .attr('y2', function(d) { return my.y_scale(d['const'] + x2*d.slope); });
         
-
       line.exit()
         .remove();
-        
+    }
+    
+    my.draw_axes = function() {
+      var xticks, xticks, x_sorted, y_sorted, quantiles, xquant, yquant,
+        xlabels, ylabels;
+      quantiles = [0, 0.25, 0.5, 0.75, 1];
+      x_sorted = my.xvals.slice().sort(d3.ascending);
+      y_sorted = my.yvals.slice().sort(d3.ascending);
+      xquant = quantiles.map(function(q) {return d3.quantile(x_sorted, q)});
+      yquant = quantiles.map(function(q) {return d3.quantile(y_sorted, q)});
+      console.log(x_sorted);
+      console.log(xquant);
+      
+      xticks = my.xaxis_canvas.selectAll("line.tick")
+        .data(my.point_data, function(d) { return d.row_id; });
+      
+      xticks.enter().append('svg:line')
+        .attr('class', 'tick')
+        .attr('x1', function(d) { return my.x_scale(d.x); })
+        .attr('x2', function(d) { return my.x_scale(d.x); })
+        .attr('y1', 0)
+        .attr('y2', -7)
+        .attr('stroke', 'black')
+        .attr('stroke-opacity', 0.2);
+
+      xticks.transition()
+        .duration(my.duration)
+        .attr('x1', function(d) { return my.x_scale(d.x); })
+        .attr('x2', function(d) { return my.x_scale(d.x); });
+      
+      xticks.exit()
+        .remove();
+      
+      yticks = my.yaxis_canvas.selectAll("line.tick")
+        .data(my.point_data, function(d) { return d.row_id; });
+      
+      yticks.enter().append('svg:line')
+        .attr('class', 'tick')
+        .attr('y1', function(d) { return my.y_scale(d.y); })
+        .attr('y2', function(d) { return my.y_scale(d.y); })
+        .attr('x1', 0)
+        .attr('x2', 7)
+        .attr('stroke', 'black')
+        .attr('stroke-opacity', 0.2);
+
+      yticks.transition()
+        .duration(my.duration)
+        .attr('y1', function(d) { return my.y_scale(d.y); })
+        .attr('y2', function(d) { return my.y_scale(d.y); });
+      
+      yticks.exit()
+        .remove();
+      
+      // Now we add quantile labels
+      xlabels = my.xaxis_canvas.selectAll("g.quantile")
+        .data(xquant);
+
+      xlabels.enter().append("svg:g")
+        .attr('class', 'quantile')
+        .attr('transform', function(d) {
+          return 'translate('+my.x_scale(d)+', 10)';})
+        .append('svg:text');
+      
+      xlabels.selectAll('text').text(function(d) {return d; })
+
+      xlabels.transition()
+        .duration(my.duration)
+        .attr('transform', function(d) {
+          return 'translate('+my.x_scale(d)+', 10)';})
+        .select('text').text(function(d) {return d; });
+      
+      
+      //my.yaxis_canvas.selectAll("g.quantile")
+      //  .data(yquant)
+      //.enter().append("svg:g")
+      //  .attr('class', 'quantile')
+      //  .attr('transform', function(d) {
+      //    return 'translate('+my.x_scale(d)+', 10)';
+      //  })
+      //  .append('svg:text').text(function(d) {return d.toFixed(2);});
+
     }
     
     console.log("hello");
